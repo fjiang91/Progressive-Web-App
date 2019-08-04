4813369eb58375141ebf18a98c3e566807080456
Feng Jiang <fjiang91@gmail.com>
2019-08-03 23:56:04 -0400

    feat: added dynamic caching and fixing caching issues;

--- sw.js
+++ sw.js
@@ -1,33 +1,52 @@
 const staticCacheName = 'site-static';
-const asset = [
+const dynamicCache = 'site-dynamic-v1';
+const assets = [
   '/',
   '/index.html',
   '/js/app.js',
   '/js/ui.js',
-  '/js/materialize.min.css',
-  '/css/style.css',
+  '/js/materialize.min.js',
+  '/css/styles.css',
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
 ];
 
-//Install Service Worker
+// install event
 self.addEventListener('install', event => {
+  //console.log('service worker installed');
   event.waitUntil(
     caches.open(staticCacheName).then(cache => {
-      console.log('Caching Shell Assets');
-      cache.addAll(asset);
+      console.log('caching shell assets');
+      cache.addAll(assets);
     })
   );
-  // console.log('serivce worker has been installed', event);
 });
 
 //Activate Service Worker
 self.addEventListener('activate', event => {
-  console.log('Service Worker has been activated', event);
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 //Fetch Event
 self.addEventListener('fetch', event => {
-  console.log('Fetch Event', event);
+  event.respondWith(
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request).then( fetchRes => {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+        });
+      });
+    })
+  );
 });
